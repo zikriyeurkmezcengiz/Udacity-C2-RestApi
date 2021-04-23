bf36d61ea7cf92c6578988b31fa38867f596395a
Zikriye Urkmez Cengiz <zikriyeurkmez@gmail.com>
2021-04-23 08:09:24 +0300

    add endpoints

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramcengizdev",
+    "password": "121214",
+    "database": "udagramcengizdev",
+    "host": "udagramcengizdev.c5s8irweq7ba.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
